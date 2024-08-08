22fdbf772f91fce6b271196d172b3e25cfda93a7
NeuralNoble <coursesxyz403@gmail.com>
2024-08-08 14:02:34 +0530

    did ball interpolation , assigned teams to players , draw ellipses at the foot of players

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import cv2
 from utils import read_video , save_video
 from trackers import  Tracker
-
+from team_assigner import TeamAssigner
 def main():
     video_frames = read_video("./input_videos/08fd33_4.mp4")
 
-    save_video(video_frames,"./output_videos/output_video.avi")
+    #intialize tracker
+
+    tracker = Tracker()
+    tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
+
+    # interpolate ball positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Assign player teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
+
+    for frame_num , player_track in enumerate(tracks['players']):
+        for player_id ,track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],track['bbox'],player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    output_video_frames = tracker.draw_annotations(video_frames,tracks)
+
+    save_video(output_video_frames,"./output_videos/output_video.avi")
 
 
 if __name__ == '__main__':
---
+++ utils/bbox_utils.py
@@ -0,0 +1,8 @@
+def get_centre(bbox):
+    x1 ,y1,x2,y2 = bbox
+
+    return int((x1+x2)/2) , int((y1+y2)/2)
+
+
+def get_width(bbox):
+    return bbox[2]-bbox[0]
